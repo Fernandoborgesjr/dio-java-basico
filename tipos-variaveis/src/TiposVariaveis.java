59bea38982161809daf6203ff6263cf41f5b53f7
Fernando Borges <fernandomborges96@gmail.com>
2024-05-21 21:48:48 -0300

    feat: add "TiposVariaveis"

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,28 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        // Inteiros
+        int idade = 25;
+        byte valor = 100;
+        short numero = 5000;
+        long populacaoMundial = 7800000000L;
+
+        // Ponto flutuante
+        float altura = 1.75F;
+        double salario = 3500.50;
+
+        // Booleano
+        boolean estaChovendo = true;
+
+        // Caractere
+        char letra = 'A';
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Valor: " + valor);
+        System.out.println("Número: " + numero);
+        System.out.println("População Mundial: " + populacaoMundial);
+        System.out.println("Altura: " + altura);
+        System.out.println("Salário: " + salario);
+        System.out.println("Está chovendo? " + estaChovendo);
+        System.out.println("Letra: " + letra);
+    }
+}
